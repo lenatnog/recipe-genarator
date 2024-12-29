6f7de26a79f64717baa5893dade520087307e8f3
lenatnog <helena_1306@hotmail.com>
2024-12-29 16:16:48 -0300

    Improved UX with a "generating" message, a hidden element for the recipe and an example for how to fill the form

--- src/script.js
+++ src/script.js
@@ -10,12 +10,18 @@ function displayRecipe(response) {
 function generateRecipe(event) {
   event.preventDefault();
 
-  let instuctionsInput = document.querySelector("#user-instructions-input");
+  let instructionsInput = document.querySelector("#user-instructions-input");
   let apiKey = "4ebab2t0fcb344b3cao02f08bf5ec9b1";
-  let prompt = `User instructions: generate a recipe with ${instuctionsInput.value} as the main ingredient. Don't show the HTML marks`;
+  let prompt = `User instructions: generate a recipe with ${instructionsInput.value} as the main ingredient. Don't show the HTML marks`;
   let context =
     "You are a cooking expert enthusiastic about helping people learn how to cook. Your mission is to generate a short, simple, and easy-to-follow recipe in basic HTML. Ensure you follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+
+  let lowerCaseInput = instructionsInput.value.toLowerCase();
+  recipeElement.innerHTML = `<div class="generating">Generating a recipe with ${lowerCaseInput} just for you...</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
