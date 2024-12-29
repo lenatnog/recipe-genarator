fb8c7c6e9f29ffc2e0fc329331900c931c93be00
lenatnog <helena_1306@hotmail.com>
2024-12-29 13:48:07 -0300

    Added basic form handling and typewriter effect on JavaScript

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Ingredients and recipe",
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
